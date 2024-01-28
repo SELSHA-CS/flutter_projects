956756f608b8e2eb748650b28d6be8c1ed417f13
SELSHA-CS <selshacs7@gmail.com>
2024-01-28 16:07:15 +0530

    commit

--- lib/LOGIN AND SIGNUP/login.dart
+++ lib/LOGIN AND SIGNUP/login.dart
@@ -32,73 +32,76 @@ class _LoginState extends State<Login> {
           padding: const EdgeInsets.all(8.0),
           child: Form(
             key: Form_Key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  controller: uname,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: uname,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      hintText: "Email",
                     ),
-                    hintText: "Email",
                   ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  controller: pswd,
-                  obscureText: pw,
-                  decoration: InputDecoration(
-                    suffixIcon: IconButton(
-                      onPressed: (){
-                        setState(() {
-                          if(pw == true){
-                            pw = false;
-                          }
-                          else{
-                            pw = true;
-                          }
-                        });
-                      }, 
-                      icon: Icon(pw == true ? Icons.visibility_off : Icons.visibility)
+                  SizedBox(height: 10,),
+                  TextFormField(
+                    controller: pswd,
+                    obscureText: pw,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: (){
+                          setState(() {
+                            if(pw == true){
+                              pw = false;
+                            }
+                            else{
+                              pw = true;
+                            }
+                          });
+                        }, 
+                        icon: Icon(pw == true ? Icons.visibility_off : Icons.visibility)
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      hintText: "Password",
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    hintText: "Password",
                   ),
-                ),
-                SizedBox(height: 10,),
-                ElevatedButton(
-                  onPressed: (){
-                    if(uname.text == "admin" && pswd.text == "1234"){
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => Home())
+                  SizedBox(height: 10,),
+                  ElevatedButton(
+                    onPressed: (){
+                      if(uname.text == "admin" && pswd.text == "1234"){
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Home())
+                        );
+                      }
+                      else{
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("Invalid login credentials"))
+                        );
+                      }
+                    }, 
+                    child: Text("Login", style: TextStyle(color: Colors.white),),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: (){
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => Register())
                       );
-                    }
-                    else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("Invalid login credentials"))
-                      );
-                    }
-                  }, 
-                  child: Text("Login", style: TextStyle(color: Colors.white),),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
+                    }, 
+                    child: Text("Register", style: TextStyle(color: Colors.white)),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
                   ),
-                ),
-                TextButton(
-                  onPressed: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => Register())
-                    );
-                  }, 
-                  child: Text("Register", style: TextStyle(color: Colors.white)),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
